a95c7a5a27f0565e28d60ff8b8b9f9b7703c39b5
Gary_Huang <deryann@gmail.com>
2021-10-06 14:19:38 +0800

    Feat: add json request sample
    Add request example.

--- client/request_client.py
+++ client/request_client.py
@@ -2,6 +2,25 @@ import requests
 import os
 
 
+def call_json_req():
+    scoring_uri = "http://127.0.0.1:5000/api_group_a/echo"
+    headers = {'Content-Type': 'application/json'}
+
+    rawdata = {"df_input": "001", }
+    raw_str = json.dumps(rawdata)
+    print("input_data json = {}".format(raw_str))
+    response = requests.post(scoring_uri, data=raw_str, headers=headers)
+    print(response.status_code)
+    print(response.elapsed)
+    dic_respon = response.json()
+
+    print(dic_respon.keys())
+    print("response json {}".format(dic_respon))
+
+    print()
+    return dic_respon
+
+
 def test_multiple_file():
     """
     test multiple file format upload and download
